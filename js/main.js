d9de2f5f4f996bde00d2176de8ab7361fac11039
webmanufactory <bubunya@yandex.ru>
2023-07-05 17:53:00 +0300

    new changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 let input = document.querySelector('.phone');
 document.querySelector('.button-input').addEventListener('click', () => {
-  document.querySelector('.telegram__link').href = "https://t.me/" + (input.value[0] = '+7' + input.value.slice(1).replaceAll(/[^0-9\+]/g, ''));
-  document.querySelector('.whatsapp__link').href = "https://wa.me/" + (input.value[0] = '+7' + input.value.slice(1).replaceAll(/[^0-9\+]/g, ''));
+  if (input.value[0] == "+7") {
+    document.querySelector('.telegram__link').href = "https://t.me/" + input.value.replaceAll(/[^0-9\+]/g, '');
+    document.querySelector('.whatsapp__link').href = "https://wa.me/" + input.value.replaceAll(/[^0-9\+]/g, '');
+  } else if (input.value[0] == "+") {
+    document.querySelector('.telegram__link').href = "https://t.me/" + input.value.replaceAll(/[^0-9\+]/g, '');
+    document.querySelector('.whatsapp__link').href = "https://wa.me/" + input.value.replaceAll(/[^0-9\+]/g, '');
+  } else {
+    document.querySelector('.telegram__link').href = "https://t.me/" + (input.value[0] = '+7' + input.value.slice(1).replaceAll(/[^0-9\+]/g, ''));
+    document.querySelector('.whatsapp__link').href = "https://wa.me/" + (input.value[0] = '+7' + input.value.slice(1).replaceAll(/[^0-9\+]/g, ''));
+  }
+  input.value = '';
 })
